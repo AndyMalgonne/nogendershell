37e41902081e9200a8ab59969ecf61e1f36c917b
Jacket913 <>
2024-10-30 18:26:08 +0100

    Add tokens

---
+++ include/tokens.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokens.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmoulin <gmoulin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 16:16:43 by gmoulin           #+#    #+#             */
+/*   Updated: 2024/10/24 16:19:08 by gmoulin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TOKENS_H
+# define TOKENS_H
+
+# include "minishell.h"
+
+t_token	*new_token(t_token_type type, char *value);
+void	append_token(t_token **head, t_token *new_token);
+
+#endif

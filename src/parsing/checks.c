37bf9dd4d843cb2963f77b575b4f78e7d8d84339
Jacket913 <>
2024-10-30 18:25:23 +0100

    Add checks.c quotes_check

---
+++ src/parsing/checks.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checks.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmoulin <gmoulin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 15:00:00 by gmoulin           #+#    #+#             */
+/*   Updated: 2024/10/30 15:10:52 by gmoulin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	check_quotes(char *rl)
+{
+	int	i;
+	int	double_quote;
+	int	single_quote;
+
+	i = 0;
+	double_quote = 0;
+	single_quote = 0;
+	while (rl[i])
+	{
+		if (rl[i] == '\"')
+			double_quote++;
+		if (rl[i] == '\'')
+			single_quote++;
+		i++;
+	}
+	if (double_quote % 2 != 0 || single_quote % 2 != 0)
+	{
+		ft_printf("nogendershell: syntax error: open quotes.\n");
+		return (1);
+	}
+	return (0);
+}

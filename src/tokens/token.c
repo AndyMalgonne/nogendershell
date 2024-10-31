776492566a4fe0bd1feaaaad6105c467325c5b41
Jacket913 <>
2024-10-31 19:44:38 +0100

    Add temp get_next_token() token.c

--- src/tokens/token.c
+++ src/tokens/token.c
@@ -6,8 +6,22 @@
 /*   By: gmoulin <gmoulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 13:19:22 by gmoulin           #+#    #+#             */
-/*   Updated: 2024/10/30 18:17:14 by gmoulin          ###   ########.fr       */
+/*   Updated: 2024/10/31 19:22:17 by gmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	get_next_token(const char *rl, t_token *token)
+{
+	char	buffer[4096];
+
+	while (*rl && is_space_tab(*rl))
+		rl++;
+	if (!*rl)
+	{
+		token = new_token(END, NULL);
+		return (0);
+	}
+}
+// WIP function, not sure if even useful yet^

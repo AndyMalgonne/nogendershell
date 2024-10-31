ac9aa1059025e6c1c01e90b1c97d156740129f43
andymalgonne <andymalgonne@gmail.com>
2024-10-31 09:30:53 +0100

    add and finish pwd.c

---
+++ src/builtin/pwd.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andymalgonne <andymalgonne@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 08:30:59 by andymalgonn       #+#    #+#             */
+/*   Updated: 2024/10/31 09:28:18 by andymalgonn      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	bi_pwd(void)
+{
+	char	*cwd[PATH_MAX];
+	
+	if(getcwd(cwd, PATH_MAX))
+		return (ft_putendl_fd(cwd, 1), 0);
+	else
+		return(ft_putendl_fd(strerror(errno), 2), 1);
+}

a9aa33305d96207e91e186ca12f5f987f2ff7a66
andymalgonne <andymalgonne@gmail.com>
2024-10-31 10:01:41 +0100

    fix bi.h & add rule .gitignore

--- include/builtin.h
+++ include/builtin.h
@@ -6,7 +6,7 @@
 /*   By: andymalgonne <andymalgonne@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 08:46:06 by andymalgonn       #+#    #+#             */
-/*   Updated: 2024/10/31 08:49:24 by andymalgonn      ###   ########.fr       */
+/*   Updated: 2024/10/31 09:48:11 by andymalgonn      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 
 # include "minishell.h"
 
-int	bi_pwd(int fd_out);
+int	bi_pwd(void);
 
 #endif

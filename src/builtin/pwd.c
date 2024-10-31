8f2d24193f93e81d1f3e882ec513207fdd90e0db
andymalgonne <andymalgonne@gmail.com>
2024-10-31 11:06:05 +0100

    try smth

--- src/builtin/pwd.c
+++ src/builtin/pwd.c
@@ -6,7 +6,7 @@
 /*   By: andymalgonne <andymalgonne@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 08:30:59 by andymalgonn       #+#    #+#             */
-/*   Updated: 2024/10/31 09:34:02 by andymalgonn      ###   ########.fr       */
+/*   Updated: 2024/10/31 10:37:21 by andymalgonn      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	bi_pwd(void)
 {
-	char	*cwd[PATH_MAX];
+	char	cwd[PATH_MAX];
 
 	if (getcwd(cwd, PATH_MAX))
 		return (ft_putendl_fd(cwd, 1), 0);

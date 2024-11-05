a1386a94bc918f068003f3e380451fe47d96b202
Jacket913 <>
2024-11-05 16:49:10 +0100

    Fixes conditional blocks

--- src/parsing/operator_tokenizing.c
+++ src/parsing/operator_tokenizing.c
@@ -6,7 +6,7 @@
 /*   By: gmoulin <gmoulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 17:14:57 by gmoulin           #+#    #+#             */
-/*   Updated: 2024/11/05 16:29:10 by gmoulin          ###   ########.fr       */
+/*   Updated: 2024/11/05 16:45:01 by gmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,19 @@ int	operator_tokenizing(char **rl, t_token **head)
 	t_token	*n_token;
 
 	if (is_rdin(**rl))
+	{
 		if (is_rdin(**rl + 1))
 			n_token = new_token(REDIR_APPEND, ft_strdup(">>"));
 		else
 			n_token = new_token(REDIR_IN, ft_strdup(">"));
+	}
 	else if (is_rdout(**rl))
+	{
 		if (is_rdout(**rl + 1))
 			n_token = new_token(REDIR_HEREDOC, ft_strdup("<<"));
 		else
 			n_token = new_token(REDIR_OUT, ft_strdup("<"));
+	}
 	else if (is_pipe(**rl))
 		n_token = new_token(PIPE, ft_strdup("|"));
 	else

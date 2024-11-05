7afdd4422b3346331c2ca07c119b3f333d86ff96
Jacket913 <>
2024-11-05 16:35:46 +0100

    Remove unsupported token types parsing

--- src/parsing/operator_tokenizing.c
+++ src/parsing/operator_tokenizing.c
@@ -6,7 +6,7 @@
 /*   By: gmoulin <gmoulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 17:14:57 by gmoulin           #+#    #+#             */
-/*   Updated: 2024/10/31 19:10:39 by gmoulin          ###   ########.fr       */
+/*   Updated: 2024/11/05 16:29:10 by gmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,24 @@ int	operator_tokenizing(char **rl, t_token **head)
 {
 	t_token	*n_token;
 
-	n_token = new_token(WORD, ft_strdup(*rl));
-	if (!n_token)
-		return (0);
-	if (is_redir_in(**rl))
-		n_token->type = REDIR_IN;
-	else if (is_redir_out(**rl))
-		n_token->type = REDIR_OUT;
+	if (is_rdin(**rl))
+		if (is_rdin(**rl + 1))
+			n_token = new_token(REDIR_APPEND, ft_strdup(">>"));
+		else
+			n_token = new_token(REDIR_IN, ft_strdup(">"));
+	else if (is_rdout(**rl))
+		if (is_rdout(**rl + 1))
+			n_token = new_token(REDIR_HEREDOC, ft_strdup("<<"));
+		else
+			n_token = new_token(REDIR_OUT, ft_strdup("<"));
 	else if (is_pipe(**rl))
-		n_token->type = PIPE;
-	else if (is_semicolon(**rl))
-		n_token->type = SEMICOLON;
-	else if (is_and(**rl))
-		n_token->type = AND;
+		n_token = new_token(PIPE, ft_strdup("|"));
 	else
 		return (0);
+	if (!n_token)
+		return (0);
 	append_token(head, n_token);
-	(*rl)++;
+	while (**rl && !is_space_tab(**rl))
+		(*rl)++;
 	return (1);
 }

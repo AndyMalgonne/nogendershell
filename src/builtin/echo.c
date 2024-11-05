7a5b2013720a8ad18ba34957337c41b4699530db
andymalgonne <andymalgonne@gmail.com>
2024-11-05 09:24:48 +0100

    finish echo.c

---
+++ src/builtin/echo.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andymalgonne <andymalgonne@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 09:17:34 by andymalgonn       #+#    #+#             */
+/*   Updated: 2024/11/05 09:18:41 by andymalgonn      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	nb_args(char **args)
+{
+	int	i;
+
+	i = 0;
+	while (args[i])
+		i++;
+	return (i);
+}
+
+int	ft_echo(char **args)
+{
+	int	i;
+	int	flag;
+
+	i = 1;
+	flag = 0;
+	if (nb_args(args) > 1 && !ft_strcmp(args[1], "-n"))
+	{
+		flag = 1;
+		i++;
+	}
+	while (args[i])
+	{
+		ft_printf("%s", args[i]);
+		if (args[i + 1])
+			ft_printf(" ");
+		i++;
+	}
+	if (!flag)
+		ft_printf("\n");
+	return (0);
+}

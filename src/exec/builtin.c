381624906f6716b23d970042bb3e4c52705d82c3
andymalgonne <andymalgonne@gmail.com>
2025-01-30 15:22:52 +0100

    put builtin in right place

--- src/exec/builtin.c
+++ src/exec/builtin.c
@@ -3,42 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   builtin.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
+/*   By: andymalgonne <andymalgonne@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 09:59:16 by andymalgonn       #+#    #+#             */
-/*   Updated: 2025/01/30 14:22:32 by abasdere         ###   ########.fr       */
+/*   Updated: 2025/01/30 15:12:34 by andymalgonn      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// bool	is_builtin(char *cmd)
-// {
-// 	if (!cmd)
-// 		return (false);
-// 	if (!ft_strcmp("echo", cmd) || !ft_strcmp("cd", cmd)
-// 	|| !ft_strcmp("pwd", cmd) || !ft_strcmp("export", cmd)
-// 	|| !ft_strcmp("unset", cmd) || !ft_strcmp("env", cmd)
-// 	|| !ft_strcmp("exit", cmd))
-// 		return (true);
-// 	return (false);
-// }
+bool	is_builtin(char *cmd)
+{
+	if (!cmd)
+		return (false);
+	if (!ft_strcmp("echo", cmd) || !ft_strcmp("cd", cmd)
+	|| !ft_strcmp("pwd", cmd) || !ft_strcmp("export", cmd)
+	|| !ft_strcmp("unset", cmd) || !ft_strcmp("env", cmd)
+	|| !ft_strcmp("exit", cmd))
+		return (true);
+	return (false);
+}
 
-// int	exec_builtin(char **args)
-// {
-// 	if (!ft_strcmp("echo", args[0]))
-// 		return (ft_echo(args));
-// 	if (!ft_strcmp("cd", args[0]))
-// 		return (ft_cd(args));
-// 	if (!ft_strcmp("pwd", args[0]))
-// 		return (ft_pwd());
-// 	if (!ft_strcmp("export", args[0]))
-// 		return (ft_export(args));
-// 	if (!ft_strcmp("unset", args[0]))
-// 		return (ft_unset(args));
-// 	if (!ft_strcmp("env", args[0]))
-// 		return (ft_env());
-// 	if (!ft_strcmp("exit", args[0]))
-// 		return (ft_exit(args));
-// 	return (0);
-// }
+int	exec_builtin(char **args)
+{
+	if (!ft_strcmp("echo", args[0]))
+		return (ft_echo(args));
+	if (!ft_strcmp("cd", args[0]))
+		return (ft_cd(args));
+	if (!ft_strcmp("pwd", args[0]))
+		return (ft_pwd());
+	if (!ft_strcmp("export", args[0]))
+		return (ft_export(args));
+	if (!ft_strcmp("unset", args[0]))
+		return (ft_unset(args));
+	if (!ft_strcmp("env", args[0]))
+		return (ft_env());
+	if (!ft_strcmp("exit", args[0]))
+		return (ft_exit(args));
+	return (0);
+}

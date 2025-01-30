a587e8a392274512cd2bb0990c0dcfd5d6d68d21
andymalgonne <andymalgonne@gmail.com>
2025-01-30 16:08:35 +0100

    remove commented code for norminette

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,27 +6,30 @@
 /*   By: andymalgonne <andymalgonne@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 08:38:56 by andymalgonn       #+#    #+#             */
-/*   Updated: 2025/01/30 14:35:41 by andymalgonn      ###   ########.fr       */
+/*   Updated: 2025/01/30 15:32:09 by andymalgonn      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void exec_command(t_tree *tree)
+void	exec_command(t_tree *tree)
 {
-	pid_t pid = fork();
+	pid_t	pid;
 
-	// if (!tree || !tree->cmd)
-	//     return;
-	if (pid == -1) {
+	pid = fork();
+	if (pid == -1)
+	{
 		perror("fork");
 		exit(EXIT_FAILURE);
 	}
-	if (pid == 0) {
+	if (pid == 0)
+	{
 		execve(tree->cmd[0], tree->cmd, NULL);
 		perror("execvp");
 		exit(EXIT_FAILURE);
-	} else {
+	}
+	else
+	{
 		wait(NULL);
 	}
 }

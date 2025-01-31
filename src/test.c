46498037d0daeddb30a6e116b2519ae367e8a066
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2025-01-31 13:53:29 +0100

    norm test.c

--- src/test.c
+++ src/test.c
@@ -1,7 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 13:49:37 by abasdere          #+#    #+#             */
+/*   Updated: 2025/01/31 13:52:13 by abasdere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-t_tree *test_cat_makefile() {
-	t_tree *tree;
+void	test_cat_makefile(void)
+{
+	t_tree	*tree;
 
 	tree = malloc(sizeof(t_tree));
 	tree->cmd = ft_split("cat Makefile", ' ');
@@ -10,56 +23,51 @@ t_tree *test_cat_makefile() {
 }
 // cat Makefile
 
-t_tree *test_cat_mult_infile() {
-	t_tree *tree;
+void	test_cat_mult_infile(void)
+{
+	t_tree	*tree;
 
 	tree = malloc(sizeof(t_tree));
 	tree->cmd = ft_split("cat", ' ');
-
 	tree->io = malloc(sizeof(t_iofile));
 	tree->io->type = INFILE;
 	tree->io->value = "Makefile";
-
 	tree->io->next = malloc(sizeof(t_iofile));
 	tree->io->next->type = INFILE;
 	tree->io->next->value = ".gitignore";
 	tree->io->next->next = NULL;
-
 	tree->io = NULL;
 	tree->next = NULL;
 }
 // cat < Makefile < .gitignore
 
-t_tree *test_cat_mult_outfile(){
-	t_tree *tree;
+void	*test_cat_mult_outfile(void)
+{
+	t_tree	*tree;
 
 	tree = malloc(sizeof(t_tree));
 	tree->cmd = ft_split("cat", ' ');
-
 	tree->io = malloc(sizeof(t_iofile));
 	tree->io->type = OUTFILE_TRUNC;
 	tree->io->value = "test.txt";
-
 	tree->io->next = malloc(sizeof(t_iofile));
 	tree->io->next->type = OUTFILE_APPEND;
 	tree->io->next->value = "test2.txt";
 	tree->io->next->next = NULL;
-
 	tree->io = NULL;
 	tree->next = NULL;
 }
 // cat > test >> test2
 
-
-t_tree *test_ultime(){
-	t_tree *tree;
+void	test_ultime(void)
+{
+	t_tree	*tree;
 
 	tree = malloc(sizeof(t_tree));
 	tree->cmd = ft_split("cat", ' ');
 	tree->next = malloc(sizeof(t_tree));
 	tree->next->cmd = ft_split("cat", ' ');
 	tree->next->next = NULL;
-
 	tree->io = malloc(sizeof(t_iofile));
 	tree->io->type = INFILE;
 	tree->io->value = "infile1";

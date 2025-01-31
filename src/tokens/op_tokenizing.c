f77336f0cb4f33ba2b6b1119a5297a85ce210497
Jacket913 <>
2025-01-31 13:30:59 +0100

    Remove functions rdin rdout

--- src/tokens/op_tokenizing.c
+++ src/tokens/op_tokenizing.c
@@ -6,7 +6,7 @@
 /*   By: gmoulin <gmoulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 17:14:57 by gmoulin           #+#    #+#             */
-/*   Updated: 2025/01/30 16:33:20 by gmoulin          ###   ########.fr       */
+/*   Updated: 2025/01/30 16:49:16 by gmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ t_token	*tokenize_rdin(char **rl)
 {
 	if (is_space_tab((*rl)[1]) || !(*rl)[1])
 		return (new_token(REDIR_IN, ft_strdup(">")));
-	else if (is_rdin((*rl)[1]) && (is_space_tab((*rl)[2]) || !(*rl)[2]))
+	else if ((*rl)[1] == '<' && (is_space_tab((*rl)[2]) || !(*rl)[2]))
 		return (new_token(REDIR_APPEND, ft_strdup(">>")));
 	return (NULL);
 }
@@ -27,7 +27,7 @@ t_token	*tokenize_rdout(char **rl)
 {
 	if (is_space_tab((*rl)[1]) || !(*rl)[1])
 		return (new_token(REDIR_OUT, ft_strdup("<")));
-	else if (is_rdout((*rl)[1]) && (is_space_tab((*rl)[2]) || !(*rl)[2]))
+	else if ((*rl)[1] == '>' && (is_space_tab((*rl)[2]) || !(*rl)[2]))
 		return (new_token(REDIR_HEREDOC, ft_strdup("<<")));
 	return (NULL);
 }
@@ -44,11 +44,11 @@ int	op_tokenizing(char **rl, t_token **head)
 	t_token	*n_token;
 
 	n_token = NULL;
-	if (is_rdin(**rl))
+	if (**rl == '<')
 		n_token = tokenize_rdin(rl);
-	else if (is_rdout(**rl))
+	else if (**rl == '>')
 		n_token = tokenize_rdout(rl);
-	else if (is_pipe(**rl))
+	else if (**rl == '|')
 		n_token = tokenize_pipe(rl);
 	else
 		return (0);

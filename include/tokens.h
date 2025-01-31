0fa2aa0ee32bc1308dec2abd713314f03bd2c4bc
Jacket913 <>
2025-01-31 16:00:52 +0100

    Clean tokens.h

--- include/tokens.h
+++ include/tokens.h
@@ -6,7 +6,7 @@
 /*   By: gmoulin <gmoulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 16:16:43 by gmoulin           #+#    #+#             */
-/*   Updated: 2025/01/31 14:53:02 by gmoulin          ###   ########.fr       */
+/*   Updated: 2025/01/31 15:45:50 by gmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,17 +35,6 @@ typedef struct s_token
 	struct s_token	*prev;
 }	t_token;
 
-//op_tokenizing.c
-t_token		*tokenize_rdin(char **rl);
-t_token		*tokenize_rdout(char **rl);
-t_token		*tokenize_pipe(char **rl);
-int			op_tokenizing(char **rl, t_token **head);
-
-//string_tokenizing.c
-int			tokenize_s_quote(char **rl, t_token **head);
-int			tokenize_d_quote(char **rl, t_token **head);
-int			string_tokenizing(char **rl, t_token **head);
-
 //token_utils.c
 t_token		*new_token(t_token_type type, const char *value);
 void		append_token(t_token **head, t_token *new_token);
@@ -56,9 +45,8 @@ void		free_token_list(t_token **head);
 void		print_token_list(t_token *head);
 const char	*tttostr(t_token_type type);
 
-//token.c
-int			tokenize_else(char **rl, t_token **head);
-t_token		*tokenize(char *input);
+//tokenize.c
+int			tokenize(char *input, t_token **head);
 
 int			parse_input(char *user_input, t_tree **tree, t_env *env);
 

f91e01f3ec070f23e2a72d0e9f28b5a102f6e41b
andymalgonne <andymalgonne@gmail.com>
2025-02-03 09:27:57 +0100

    downgrade exec_command to make the project compile again

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,25 +6,30 @@
 /*   By: andymalgonne <andymalgonne@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 08:38:56 by andymalgonn       #+#    #+#             */
-/*   Updated: 2025/02/03 09:13:01 by andymalgonn      ###   ########.fr       */
+/*   Updated: 2025/02/03 09:25:29 by andymalgonn      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static bool	exec_cmd(t_tree *tree)
+void	exec_command(t_tree *tree)
 {
 	pid_t	pid;
-
+	
 	pid = fork();
-	if (pid < 0)
-		(perror("fork"), exit(EXIT_FAILURE));
-	else if (pid == 0)
+	if (pid == -1)
 	{
-		if (tree->cmd && tree->cmd[0])
-			child_process(tree);
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	if (pid == 0)
+	{
+		execve(tree->cmd[0], tree->cmd, NULL);
+		perror("execvp");
+		exit(EXIT_FAILURE);
 	}
 	else
-		parent_process(tree);
-	return (true);
+	{
+		wait(NULL);
+	}
 }

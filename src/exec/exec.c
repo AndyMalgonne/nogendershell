85ee37d975d95a5d8c26d35bf622076904de39f8
andymalgonne <andymalgonne@gmail.com>
2025-02-04 10:37:42 +0100

    norm exec.c

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,16 +6,16 @@
 /*   By: andymalgonne <andymalgonne@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 08:38:56 by andymalgonn       #+#    #+#             */
-/*   Updated: 2025/02/04 10:08:27 by andymalgonn      ###   ########.fr       */
+/*   Updated: 2025/02/04 10:37:17 by andymalgonn      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int io_files(t_iofile *io)
+int	io_files(t_iofile *io)
 {
-	int infd;
-	int outfd;
+	int	infd;
+	int	outfd;
 
 	infd = 1;
 	outfd = 0;
@@ -28,7 +28,7 @@ int io_files(t_iofile *io)
 		if (io->type == OUTFILE_TRUNC)
 			outfd = open(io->value, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		if (infd < 0 || outfd < 0)
-				return(perror(io->value), -1);
+			return (perror(io->value), -1);
 		io = io->next;
 	}
 	(dup2(infd, STDIN_FILENO), close(infd));
@@ -36,48 +36,46 @@ int io_files(t_iofile *io)
 	return (0);
 }
 
-void exec_cmd(t_tree *cmd, int *pip, t_var *env)
+int	exec_cmd(t_tree *cmd, int *pip, t_var *var)
 {
-    char *path;
+	char	*path;
 
-    (void)env;
-    path = "/bin/ls";
-    cmd->cmd[0] = path;
-
-    if (io_files(cmd->io) < 0)
-		return ;
-    if (cmd->next)
-    {
-        dup2(pip[1], STDOUT_FILENO);
-        close(pip[0]);
-        close(pip[1]);
-    }
-    execve(cmd->cmd[0], cmd->cmd, NULL);
-    perror("execve failed");
-    return;
+	path = "/bin/ls";
+	cmd->cmd[0] = path;
+	if (io_files(cmd->io) < 0)
+		return (set_and_return_code(var, 1));
+	if (cmd->next)
+	{
+		dup2(pip[1], STDOUT_FILENO);
+		close(pip[0]);
+		close(pip[1]);
+	}
+	execve(cmd->cmd[0], cmd->cmd, NULL);
+	perror("execve failed");
+	return (0);
 }
 
-void minishell_exec(t_tree *cmd, t_var *env)
+void	minishell_exec(t_tree *cmd, t_var *var)
 {
-    int pip[2];
-    pid_t pid;
+	int		pip[2];
+	pid_t	pid;
 
-    while (cmd)
-    {
-        if (cmd->next)
-            if (pipe(pip) == -1)
-			return(perror("pipe failed"));
-        pid = fork();
-        if (pid < 0)
-			return(perror("fork failed"));
-        if (pid == 0)
-        {
-            if (cmd->next)
-                close(pip[0]);
-            exec_cmd(cmd, pip, env);
-        }
-        close(pip[1]);
-        close(pip[0]);
-        cmd = cmd->next;
-    }
+	while (cmd)
+	{
+		if (cmd->next)
+			if (pipe(pip) == -1)
+				return (perror("pipe failed"));
+		pid = fork();
+		if (pid < 0)
+			return (perror("fork failed"));
+		if (pid == 0)
+		{
+			if (cmd->next)
+				close(pip[0]);
+			exec_cmd(cmd, pip, var);
+		}
+		close(pip[1]);
+		close(pip[0]);
+		cmd = cmd->next;
+	}
 }

47b9a771690b11b2dd873026456e0a6de2cbd969
andymalgonne <andymalgonne@gmail.com>
2025-02-04 13:28:56 +0100

    fix norm

--- include/exec.h
+++ include/exec.h
@@ -6,11 +6,7 @@
 /*   By: andymalgonne <andymalgonne@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 08:46:06 by andymalgonn       #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/02/04 13:18:22 by andymalgonn      ###   ########.fr       */
-=======
-/*   Updated: 2025/02/04 13:26:42 by andymalgonn      ###   ########.fr       */
->>>>>>> 3e87809 (fix norm)
+/*   Updated: 2025/02/04 13:28:10 by andymalgonn      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +30,8 @@ int		exec_cmd(t_tree *cmd, const int *pip, t_var *var);
 
 // Here_doc functions
 char	*find_heredoc_file(void);
-<<<<<<< HEAD
-char	*r_name(unsigned char r_bytes[], const char alpha[], \
-int		alpha_size, int size);
-=======
 char	*r_name(unsigned char r_bytes[], const char alpha[],
 			int alpha_size, int size);
->>>>>>> 3e87809 (fix norm)
 int		get_random_bytes(unsigned char random_bytes[], int size);
 void	increment_random_bytes(unsigned char random_bytes[], int alphanum_size);
 

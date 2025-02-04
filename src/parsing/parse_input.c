101913c3414802ed76f9254ff50db4c11e2fc448
Jacket913 <>
2025-02-04 14:24:07 +0100

    Remove print_token_list()

--- src/parsing/parse_input.c
+++ src/parsing/parse_input.c
@@ -22,9 +22,7 @@ int	parse_input(char *user_input, t_tree **tree, t_var *var)
 		return (1);
 	if (!tokenize(user_input, &tokens) && error(var, "Malloc error", 1))
 		return (0);
-	print_token_list(tokens);
 	if (!expand(tokens, var->env) && error(var, "Malloc error", 1))
 		return (0);
-	print_token_list(tokens);
 	return (1);
 }

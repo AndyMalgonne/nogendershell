7f4b9ce0d02fc8ace92d5ba800ddc4abfd72d65a
Jacket913 <>
2025-02-11 22:04:13 +0100

    Norm minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: andytropmimi <andytropmimi@student.42.f    +#+  +:+       +#+        */
+/*   By: gmoulin <gmoulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 13:33:38 by andymalgonn       #+#    #+#             */
-/*   Updated: 2025/02/10 10:30:49 by andytropmim      ###   ########.fr       */
+/*   Updated: 2025/02/11 22:00:45 by gmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,18 +39,19 @@
 
 extern int			g_signal;
 
-int		create_env(t_env **env, char **envp);
-void	free_env(t_env **env);
-void	print_env(const t_env *env);
-int		get_input(char **user_input);
-void	cleanup_user_input(char **user_input);
-void	set_up_var(t_var *var);
-void	free_var(t_var *var);
-int		set_and_return_code(t_var *var, int code);
+int			create_env(t_env **env, char **envp);
+void		free_env(t_env **env);
+void		print_env(const t_env *env);
+int			get_input(char **user_input);
+void		cleanup_user_input(char **user_input);
+void		set_up_var(t_var *var);
+void		free_var(t_var *var);
+int			set_and_return_code(t_var *var, int code);
 
 // t_env
-char	*get_env_value(t_env *env, const char *key);
-char	**get_path_from_env(t_env *env, int *error);
+char		*get_env_value(t_env *env, const char *key);
+char		**get_path_from_env(t_env *env, int *error);
+
 //t_iofile
 t_iofile	*new_iofile(t_iotype type, char *value);
 void		append_iofile(t_iofile **head, t_iofile *new);
@@ -65,8 +66,8 @@ int			create_tree(t_tree **tree, t_token *tokens);
 void		free_iofile(t_iofile **io);
 
 // utils.c
-void	*free_to_null(void *p);
-int		mclose(int *fd);
-int		error(t_var *var, const char *msg, int code);
+void		*free_to_null(void *p);
+int			mclose(int *fd);
+int			error(t_var *var, const char *msg, int code);
 
 #endif

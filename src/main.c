804b38cbc34ea58ee38c73c510d0009a75f2c9f0
Jacket913 <>
2025-02-21 00:13:24 +0100

    Merge changes and split main loop to its own function

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gmoulin <gmoulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 13:02:33 by andymalgonn       #+#    #+#             */
-/*   Updated: 2025/02/20 20:38:19 by gmoulin          ###   ########.fr       */
+/*   Updated: 2025/02/21 00:09:26 by gmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,13 @@ void	signal_exit_status(t_var *var)
 
 void	main_loop(t_var *var, char **user_input, t_tree **tree)
 {
-	while (!g_exit_flag)
+	while (get_input(user_input))
 	{
-		if (get_input(user_input) == 0)
-		{
-			if (g_exit_flag)
-				handle_eof();;
-			break ;
-		}
+		signal_exit_status(var);
+		g_exit_flag = 0;
 		if (!parse_input(*user_input, tree, var))
+			break ;
+		if (!minishell_exec(*tree, var))
 			break ;
 		free_tree(tree);
 	}
@@ -55,14 +53,5 @@ int	main(int ac, char **av __attribute__((unused)), char **envp)
 		return (1);
 	setup_signal_handlers();
 	main_loop(&var, &input, &tree);
-	while (get_input(&input))
-	{
-		if (!parse_input(input, &tree, &var))
-			break ;
-		free_to_null(&input);
-		if (!minishell_exec(tree, &var))
-			break ;
-		free_tree(&tree);
-	}
 	return (var.code);
 }

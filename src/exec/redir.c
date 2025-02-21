050ea604b6e7e1f0e35e20ac298fc7e4a605c305
AndyMalgonne <andymalgonne@gmail.com>
2025-02-21 12:43:37 +0100

    fix sonarcloud

--- src/exec/redir.c
+++ src/exec/redir.c
@@ -6,13 +6,13 @@
 /*   By: amalgonn <amalgonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 12:18:37 by amalgonn          #+#    #+#             */
-/*   Updated: 2025/02/21 12:35:20 by amalgonn         ###   ########.fr       */
+/*   Updated: 2025/02/21 12:43:13 by amalgonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	handle_fd_error(t_iofile *io, t_fds *fds)
+int	handle_fd_error(const t_iofile *io, t_fds *fds)
 {
 	if (fds->infd < 0 || fds->outfd < 0)
 	{
@@ -67,11 +67,8 @@ void	redir(t_fds *fds, int pip[2], t_tree *cmd, t_var *var)
 			(mclose(&(fds->infd)), free_all(cmd, var), exit(1));
 		mclose(&(fds->infd));
 	}
-	if (cmd->next)
-	{
-		if (dup2(pip[1], STDOUT_FILENO) == -1)
-			(mclose(&pip[1]), exit(1));
-	}
+	if (cmd->next && dup2(pip[1], STDOUT_FILENO) == -1)
+		(mclose(&pip[1]), exit(1));
 	if (fds->outfd > 1)
 	{
 		if (dup2(fds->outfd, STDOUT_FILENO) == -1)

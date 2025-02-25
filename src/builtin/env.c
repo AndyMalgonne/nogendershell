f404edec938e5561c64ea27e972861dc936810dc
andymalgonne <andymalgonne@gmail.com>
2025-02-25 18:09:50 +0100

    add bi_env

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -3,24 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
+/*   By: amalgonn <amalgonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 09:28:05 by andymalgonn       #+#    #+#             */
-/*   Updated: 2025/01/30 13:49:13 by abasdere         ###   ########.fr       */
+/*   Updated: 2025/02/25 18:02:44 by amalgonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	bi_env(t_env *env)
+int	bi_env(t_var *env)
 {
-	if (!env)
-		return (1);
-	while (env->next)
-	{
-		if (ft_strchr(env->value, '='))
-			printf("%s\n", env->value);
-		env = env->next;
-	}
-	return (0);
+	print_env(env->env);
+	return(0);
 }

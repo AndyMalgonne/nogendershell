099d65c3d621b099ee25af802c37e5895435b6a3
andymalgonne <andymalgonne@gmail.com>
2025-02-27 20:58:09 +0100

    fix protect TTY

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: amalgonn <amalgonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 13:02:33 by andymalgonn       #+#    #+#             */
-/*   Updated: 2025/02/27 17:23:04 by amalgonn         ###   ########.fr       */
+/*   Updated: 2025/02/27 20:57:03 by amalgonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,8 @@ int	main(int ac, char **av __attribute__((unused)), char **envp)
 	tree = NULL;
 	if (ac != 1)
 		return (ft_putstr_fd("Usage: ./minishell\n", 2), 1);
+	if (isatty(0) != 1)
+		return (ft_putstr_fd("i dont want to crash :(\n", 2), 1);
 	if (!create_env(&var.env, envp))
 		return (1);
 	main_loop(&var, &input, &tree);
